7e1161f9c07357c66fffcadc1d3c0aaf8afa028b
outtasight1010 <126138474+outtasight1010@users.noreply.github.com>
2023-04-25 23:16:36 -0400

    Add files via upload

--- serializers.py
+++ serializers.py
@@ -1,14 +1,10 @@
 from rest_framework import serializers
-from .models import Super
+from .models import SuperType
 
-class SuperSerializer(serializers.ModelSerializer):
+class SuperTypeSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Super
-        fields = ['id', 'name','alter_ego','primary_ability','secondary_ability','catch_phrase','type_of_super']
-        depth = 1
-
-    type_of_super = serializers.IntegerField(write_only = True)
-
-
+        model = SuperType
+        fields = ['type']
+        
         
         
